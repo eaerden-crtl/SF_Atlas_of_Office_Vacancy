cef4e3c8d890cb550fc756bf60aeb8e7ee0f1b21
Aerden <eli.aerden@yale.edu>
2025-12-23 14:16:04 -0500

    hello

--- apps/web/next-env.d.ts
+++ apps/web/next-env.d.ts
@@ -1,4 +1,5 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 
-// NOTE: This file should not be edited in order to maintain type safety within the project.
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
